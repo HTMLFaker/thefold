abcc0ff31b55acad370d7e53fc72b12557c70d02
junsang.hwang <joontop@linecorp.com>
2025-12-08 20:18:35 +0900

    a

---
+++ src/app/products/glass-folding-door/page.tsx
@@ -0,0 +1,13 @@
+export const metadata = {
+  title: '납품문의 | 더폴드',
+  description: '납품문의 | 더폴드',
+};
+
+export default function GreetingPage() {
+  return (
+    <main>
+      <h1>납품문의 | 더폴드</h1>
+      <p>납품문의 | 더폴드</p>
+    </main>
+  );
+}
