5794c8dbae39c54bdffe859b03c7d5c729ee1e16
junsang.hwang <joontop@linecorp.com>
2025-12-11 01:32:39 +0900

    a

--- src/components/domControls/MovieControl.tsx
+++ src/components/domControls/MovieControl.tsx
@@ -4,32 +4,23 @@ import { useEffect } from 'react';
 export default function MovieControl() {
   useEffect(() => {
     const handleScroll = () => {
-      const a = document.querySelector('.card_movie.glass');
-      const b = document.querySelector('.card_movie.partition');
-      const c = document.querySelector('.card_movie.the');
-
-      if (!a || !b || !c) return;
-
-      const rectA = a.getBoundingClientRect();
-      const rectB = b.getBoundingClientRect();
-      const rectC = c.getBoundingClientRect();
+      const elements = [
+        document.querySelector('.card_movie.glass'),
+        document.querySelector('.card_movie.partition'),
+        document.querySelector('.card_movie.the'),
+      ];
 
       const scrollTop = document.documentElement.scrollTop;
       const clientHeight = document.documentElement.clientHeight;
-
       const sum = scrollTop + clientHeight;
 
-      console.log(sum, rectA.top + window.scrollY);
-
-      if (rectA.top + window.scrollY <= sum - rectA.height / 2) {
-        a.classList.add('is_passed');
-      }
-      if (rectB.top + window.scrollY <= sum - rectB.height / 2) {
-        b.classList.add('is_passed');
-      }
-      if (rectC.top + window.scrollY <= sum - rectC.height / 2) {
-        c.classList.add('is_passed');
-      }
+      elements.map((dom) => {
+        if (!dom) return;
+        const rect = dom.getBoundingClientRect();
+        if (rect.top + window.scrollY <= sum - rect.height / 2) {
+          dom.classList.add('is_passed');
+        }
+      });
     };
 
     window.addEventListener('scroll', handleScroll);
